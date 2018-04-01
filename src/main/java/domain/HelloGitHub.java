9343dd53cc3dd574fca06269355841ae4c7724ce
xiaodeng <123456789@qq.com>
2018-04-01 16:00:25 +0800

    first commit

---
+++ src/main/java/domain/HelloGitHub.java
@@ -0,0 +1,10 @@
+package domain;
+
+/**
+ * Created by Administrator on 2018/4/1.
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("HelloGitHub");
+    }
+}
